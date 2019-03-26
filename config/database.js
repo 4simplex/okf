d8c04632ca0f01dc68511c5d6a1d78bfe430b223
Marcos Baldiviezo <marcos.sebastian.baldiviezo@everis.com>
2019-03-25 21:57:24 -0300

    Changed price format to ARS

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // database: 'mongodb://localhost:27017/app',
-  database: 'mongodb+srv://marcos:marcos123@cluster0-voi8e.mongodb.net/test?retryWrites=true',
+  database: 'mongodb://localhost:27017/app',
+  // database: 'mongodb+srv://marcos:marcos123@cluster0-voi8e.mongodb.net/test?retryWrites=true',
   secret: 'okayfundssecret',
 };
